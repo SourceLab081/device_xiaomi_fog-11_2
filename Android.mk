79d2909e691123ce8a7482d575313919453de216
...:::01000101:::... <58969724+SourceLab081@users.noreply.github.com>
2025-01-25 06:54:53 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -8,6 +8,11 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),fog)
 
+include $(call all-makefiles-under,$(LOCAL_PATH))
+# subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+#$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+
 #include $(call all-makefiles-under,$(LOCAL_PATH))
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
