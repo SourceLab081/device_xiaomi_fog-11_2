727afd22058be1cdeaefcb52422eeb1ce46b0005
...:::01000101:::... <58969724+SourceLab081@users.noreply.github.com>
2025-02-04 12:21:31 +0000

    correction vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,5 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Kernel fixups
-cd kernel/xiaomi/fog
-rm -rf Android.bp
-rm -rf techpack/audio/Android.bp
-cd ../../..
+rm -rf kernel/xiaomi/fog/Android.bp
+rm -rf kernel/xiaomi/fog/techpack/audio/Android.bp
