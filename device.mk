7bf1e0cea0e847556731a5d8cbb2d2a7db2044bb
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-08 04:57:34 +0700

    update device

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+ALLOW_MISSING_DEPENDENCIES := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # APEX's
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
