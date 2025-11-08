4cb62fca88c3ba9bff305be2a59d4a5d72df606c
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-08 06:09:28 +0000

    Revert "add libui_compat_layer"
    This reverts commit 5fb18ed1960cdb3ae27d0898f59452f42707f1a1.

--- device.mk
+++ device.mk
@@ -445,9 +445,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/qcom-caf/bootctrl
 
-PRODUCT_SOONG_NAMESPACES += hybris/mw/libhybris/libhybris
-PRODUCT_PACKAGES += libui_compat_layer
-
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
