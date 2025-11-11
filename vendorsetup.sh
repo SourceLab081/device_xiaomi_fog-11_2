5443e1d6e7846bc560d8bb94ec9a19b92252b633
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-11 21:30:29 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 df -h
 echo "execute vendorsetup.sh"
 curDir=`pwd`
-wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/chromium.zip;unzip -o chromium.zip -d $ANDROID_ROOT/external/chromium-webview/;rm chromium.zip 
+#wget https://github.com/SourceLab081/uploadz/releases/download/v0.0.2/chromium.zip;unzip -o chromium.zip -d $ANDROID_ROOT/external/chromium-webview/;rm chromium.zip 
   
