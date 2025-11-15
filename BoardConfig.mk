b9ecb95f54ddac9b8e41f6a524c0ee86eb638341
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-15 14:02:35 +0700

    Fix error contain space

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,7 @@ BOARD_KERNEL_CMDLINE += \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \ 
+    video=vfb:640x400,bpp=32,memsize=3072000 \
     swiotlb=2048 \
     selinux=1 \
     audit=0 \
