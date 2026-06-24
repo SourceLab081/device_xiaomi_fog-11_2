031fd037caa3948c0785b48c843b902ddbc6e194
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-06-24 19:02:23 +0700

    HAL "android.hardware.graphics.*" has a conflict
    Removed unnecessary graphics allocator and composer services from device.mk.

--- device.mk
+++ device.mk
@@ -259,11 +259,7 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor \
     libhidltransport.vendor \
     libhwbinder.vendor \
-    android.hidl.allocator@1.0.vendor \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hidl.allocator@1.0.vendor
 
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
